6c25a69ba4df5c52aefbd689d156de0759daa578
wanixarif <wanixarif@gmail.com>
2020-08-04 13:22:42 +0530

    added BindingAdapter, data binding moved to xml directly

--- app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/SleepNightAdapter.kt
@@ -32,20 +32,26 @@ class MyViewHolder private  constructor(val binding: ListItemSleepNightBinding):
 
 
     fun bind(item: SleepNight) {
-        val res=itemView.context.resources
-        binding.sleepTime.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        binding.sleepQualityText.text = convertNumericQualityToString(item.sleepQuality, res)
-        binding.sleepQualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-        )
+
+        binding.sleepNightVar=item
+        binding.executePendingBindings()
+
+        //Bind utils replaced everything below
+
+//        val res=itemView.context.resources
+//        binding.sleepTime.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+//        binding.sleepQualityText.text = convertNumericQualityToString(item.sleepQuality, res)
+//        binding.sleepQualityImage.setImageResource(
+//                when (item.sleepQuality) {
+//                    0 -> R.drawable.ic_sleep_0
+//                    1 -> R.drawable.ic_sleep_1
+//                    2 -> R.drawable.ic_sleep_2
+//                    3 -> R.drawable.ic_sleep_3
+//                    4 -> R.drawable.ic_sleep_4
+//                    5 -> R.drawable.ic_sleep_5
+//                    else -> R.drawable.ic_sleep_active
+//                }
+//        )
     }
 
     companion object {
--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/BindingUtils.kt
@@ -15,3 +15,39 @@
  */
 
 package com.example.android.trackmysleepquality.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
+
+
+@BindingAdapter("sleepQualityImage")
+fun ImageView.setSleepQualityImage(item:SleepNight?){
+    setImageResource(
+            when (item?.sleepQuality) {
+                0 -> R.drawable.ic_sleep_0
+                1 -> R.drawable.ic_sleep_1
+                2 -> R.drawable.ic_sleep_2
+                3 -> R.drawable.ic_sleep_3
+                4 -> R.drawable.ic_sleep_4
+                5 -> R.drawable.ic_sleep_5
+                else -> R.drawable.ic_sleep_active
+            }
+    )
+}
+
+@BindingAdapter("sleepQualityText")
+fun TextView.setSleepQualityText(item:SleepNight?){
+    text= item?.sleepQuality?.let { convertNumericQualityToString(it,context.resources) }
+}
+
+@BindingAdapter("sleepDuration")
+fun TextView.setSleepDuration(item:SleepNight?){
+    if (item != null) {
+        text= convertDurationToFormatted(item.startTimeMilli,item.endTimeMilli,context.resources)
+    }
+}
